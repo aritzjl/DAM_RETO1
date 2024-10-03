106734c11fb6f6bd8e4e5b591bd622d292ffa990
Ander <andertapiafer@gmail.com>
2024-10-03 10:17:45 +0200

    style: Diseñar About View

--- app/src/main/java/com/reto1/ultramarinos/views/AboutView.kt
+++ app/src/main/java/com/reto1/ultramarinos/views/AboutView.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -59,7 +60,103 @@ fun AboutContent(paddingValues: PaddingValues) {
         .fillMaxSize()
         .background(if (lightmode) MaterialTheme.colorScheme.primary
         else MaterialTheme.colorScheme.background)
-        .padding(paddingValues))
-    {}
+        .padding(paddingValues)) {
+        item {
+            Text(
+                text = "ULTRAMARINOS GREGORIO MARTIN",
+                fontWeight = FontWeight.ExtraBold,
+                fontSize = 30.sp,
+                color = if (lightmode) Color.Black else Color.White,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                textAlign = TextAlign.Center
+            )
 
+            Text(
+                text = "The Beginning",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                color = if (lightmode) Color.Black else Color.White,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                textAlign = TextAlign.Center
+            )
+
+            Text(
+                text = "In 1835, José María Gurtubay, a Bilbao merchant, made a mistake in his order to his suppliers. He wrote \"Envíenme primer barco que toque puerto de Bilbao 100 o 120 bacaladas primera superior\". However, the \"o\" was misinterpreted as a 0, and instead of receiving 100 or 120 bacaladas, he received **1,000,120**! This unexpected surplus of bacalao became the staple food for the people of Bilbao during the summer of that year, leading to the creation of many recipes.",
+                fontSize = 12.sp,
+                color = if (lightmode) Color.Black else Color.White
+            )
+
+            Text(
+                text = "The Tradition Continues",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                color = if (lightmode) Color.Black else Color.White,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                textAlign = TextAlign.Center
+            )
+
+            Text(
+                text = "In the following decades, bacalao became an essential ingredient in traditional Bilbao cuisine, and bacaladerías (dried cod shops) became a common sight in the city's streets and squares. Bilbao and bacalao were forever linked.",
+                fontSize = 12.sp,
+                color = if (lightmode) Color.Black else Color.White
+            )
+
+            Text(
+                text = "Gregorio Martín: A Visionary Entrepreneur",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                color = if (lightmode) Color.Black else Color.White,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                textAlign = TextAlign.Center
+            )
+
+            Text(
+                text = "In 1931, Gregorio Martín, a young entrepreneur from Medina del Campo, opened his own business in Artecalle 22, Bilbao. Thanks to his friendship with an importer, Gregorio innovated his business by specializing in bacalao and placed a sign that read: \"Gregorio Martín. Especialidad en bacalao remojado todos los días. Tno.- 13.707\". This marked the beginning of \"La Bacalada\" de Artecalle, a business that would become a staple in Bilbao's culinary scene.",
+                fontSize = 12.sp,
+                color = if (lightmode) Color.Black else Color.White
+            )
+
+            Text(
+                text = "Our Story Today",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                color = if (lightmode) Color.Black else Color.White,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                textAlign = TextAlign.Center
+            )
+
+            Text(
+                text = "Today, at Gregorio Martín, we continue to offer the same high-quality products, including enormous bacaladas, bulk legumes, cheeses, cured meats, preserves, wines, condiments, and chocolates. Our secret lies in the origin of our products and the personalized service our employees have provided for decades.",
+                fontSize = 12.sp,
+                color = if (lightmode) Color.Black else Color.White
+            )
+
+            Text(
+                text = "Our Goal",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                color = if (lightmode) Color.Black else Color.White,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                textAlign = TextAlign.Center
+            )
+
+            Text(
+                text = "We aim to continue providing the best products at the best prices, while maintaining our authenticity and professionalism. We believe that our personalized service and high-quality products set us apart, and we're proud to be a reference point in Bilbao's culinary scene.",
+                fontSize = 12.sp,
+                color = if (lightmode) Color.Black else Color.White
+            )
+        }
+    }
 }
