f6812deb795324bace2a97be903e4da631ac9b2b
Ander <andertapiafer@gmail.com>
2024-10-04 12:01:04 +0200

    fix: small changes

--- app/src/main/java/com/reto1/ultramarinos/Globals.kt
+++ app/src/main/java/com/reto1/ultramarinos/Globals.kt
@@ -4,10 +4,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 
 var total_cantidad by mutableIntStateOf(0)
 var is_single_column by mutableStateOf(false)
 var toolbarTitle by mutableStateOf("Inicio")
+var isLoggedIn by mutableStateOf(false)
+
 var lightmode by mutableStateOf(true)
-var isLoggedIn by   mutableStateOf(false)
+
