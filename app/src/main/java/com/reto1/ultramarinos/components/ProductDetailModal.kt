e30026daf000e9e6338374e72f71f73ad2bd0464
Aritz Jaber Lopes <aritzzjl@gmail.com>
2024-10-08 08:38:55 +0200

    TSK-46 TSK-15 feat: integrate products firebase and category filter

--- app/src/main/java/com/reto1/ultramarinos/components/ProductDetailModal.kt
+++ app/src/main/java/com/reto1/ultramarinos/components/ProductDetailModal.kt
@@ -2,6 +2,7 @@ package com.reto1.ultramarinos.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -16,21 +17,24 @@ fun ProductDetailModal(product: Product, onDismiss: () -> Unit) {
         onDismissRequest = onDismiss,
         title = { Text(text = product.title) },
         text = {
-            Column {
-                Image(
-                    painter = rememberAsyncImagePainter(product.imageUrl),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .aspectRatio(1f) // Para que sea cuadrada
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(text = product.description)
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(text = "Precio: ${product.price}€" + (if (product.unit != null) " / ${product.unit}" else ""))
-                product.offerPrice?.let {
-                    Text(text = "Precio de oferta: $it€" + (if (product.unit != null) " / ${product.unit}" else ""), color = Color.Red)
+            LazyColumn {
+                item{
+                    Image(
+                        painter = rememberAsyncImagePainter(product.imageUrl),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .aspectRatio(1f) // Para que sea cuadrada
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(text = product.category.toString())
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(text = "Precio: ${product.price}€" + (if (product.unit != null) " / ${product.unit}" else ""))
+                    product.offerPrice?.let {
+                        Text(text = "Precio de oferta: $it€" + (if (product.unit != null) " / ${product.unit}" else ""), color = Color.Red)
+                    }
                 }
+
             }
         },
         confirmButton = {
