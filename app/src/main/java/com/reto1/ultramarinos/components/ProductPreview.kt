633ade3cbb6078145f923bb235f70c9a6586826c
Aritz Jaber Lopes <aritzzjl@gmail.com>
2024-10-17 08:49:51 +0200

    TSK-64 refactor: implement absract class and deconstruction

--- app/src/main/java/com/reto1/ultramarinos/components/ProductPreview.kt
+++ app/src/main/java/com/reto1/ultramarinos/components/ProductPreview.kt
@@ -39,108 +39,96 @@ import com.reto1.ultramarinos.R
 import com.reto1.ultramarinos.models.Product
 
 @Composable
-fun ProductPreview(artwork: Product, email: String) {
+fun ProductPreview(
+    product: Product,
+    email: String
+) {
+    // Desestructuración de las propiedades de Product
+    val (
+        title,
+        description,
+        imageUrl,
+        price,
+        offerPrice,
+        unit,
+        category,
+        title_eus,
+        title_en,
+        description_eus,
+        description_en
+    ) = product
+
     var showModal by remember { mutableStateOf(false) }
     val context = LocalContext.current
+
     Box(
         modifier = Modifier
             .padding(6.dp) // Padding alrededor de la tarjeta
             .shadow(8.dp, RoundedCornerShape(16.dp)) // Sombra con bordes redondeados
             .background(MaterialTheme.colorScheme.primaryContainer, RoundedCornerShape(16.dp)) // Fondo blanco y bordes redondeados
-            //.border(2.dp, Color.White, RoundedCornerShape(16.dp)) // Borde fino
             .fillMaxWidth() // Ajustar el tamaño de la tarjeta
             .clickable {
                 showModal = !showModal
             }
     ) {
-        Column () {
-            // Contenedor para superponer el icono sobre la imagen
+        Column {
             Box {
-                // Imagen de la obra de arte
                 Image(
-                    painter = rememberAsyncImagePainter(artwork.imageUrl),
+                    painter = rememberAsyncImagePainter(imageUrl),
                     contentDescription = "Imagen de la obra de arte",
                     contentScale = ContentScale.Crop,
                     modifier = Modifier
                         .fillMaxWidth() // Llenar el ancho
                         .aspectRatio(1f) // Relación de aspecto 1:1 para que sea cuadrada
-                        .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp, bottomStart = 0.dp, bottomEnd = 0.dp))
+                        .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
                 )
-
-                // Fondo para el icono (recuadro gris oscuro)
-                Box(
-                    modifier = Modifier
-                        .size(36.dp) // Tamaño del recuadro
-                        //.background(Color(0xFF2C2C2C), RoundedCornerShape(8.dp)) // Color gris oscuro con bordes redondeados
-                        .align(Alignment.TopEnd) // Alinear en la esquina superior derecha
-                        .padding(8.dp) // Padding para ajustar la posición
-                ) {
-                    /*
-                    // Icono superpuesto
-                    Icon(
-                        painter = painterResource(id = iconResId),
-                        contentDescription = "Icono del estilo de la obra de arte",
-                        modifier = Modifier
-                            .size(24.dp) // Tamaño del icono
-                            .align(Alignment.Center), // Centrar el icono dentro del recuadro
-                        tint = Color.White // Color del icono
-                    )
-                    */
-                }
             }
 
             Row(
                 modifier = Modifier
-                    .fillMaxWidth() // Llenar el ancho
-                    //.background(Color(0xFF2C2C2C), RoundedCornerShape(8.dp)) // Color gris oscuro con bordes redondeados
+                    .fillMaxWidth()
                     .padding(horizontal = 8.dp)
                     .padding(top = 8.dp)
-                    .padding(bottom= 4.dp)
+                    .padding(bottom = 4.dp)
             ) {
-                // Título de la obra de arte
                 Text(
-                    text = artwork.title,
-                    style = MaterialTheme.typography.bodyLarge, // Usando la fuente EB Garamond definida en el tema
+                    text = title,
+                    style = MaterialTheme.typography.bodyLarge,
                     fontWeight = FontWeight.ExtraBold,
                     fontSize = 18.sp,
-                    modifier = Modifier
-                        .padding(start = 8.dp)
-                    , color = MaterialTheme.colorScheme.onPrimaryContainer // Color del texto
+                    modifier = Modifier.padding(start = 8.dp),
+                    color = MaterialTheme.colorScheme.onPrimaryContainer
                 )
             }
-            if (artwork.offerPrice != null) {
+
+            if (offerPrice != null) {
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(horizontal = 8.dp)
                         .padding(bottom = 4.dp)
                 ) {
-                    // Precio de oferta en rojo
                     Text(
-                        text = artwork.offerPrice.toString() + "€" + (if (artwork.unit!=null) "/" + artwork.unit else ""),
+                        text = "$offerPrice€${if (unit != null) "/$unit" else ""}",
                         style = MaterialTheme.typography.bodyLarge,
-                        fontSize = 18.sp,  // Tamaño más grande
-                        color = Color.Red,  // Texto en rojo
+                        fontSize = 18.sp,
+                        color = Color.Red,
                         fontWeight = FontWeight.SemiBold,
-                        modifier = Modifier
-                              // Esto lo sitúa en la izquierda
-                            .padding(start = 8.dp)
+                        modifier = Modifier.padding(start = 8.dp)
                     )
 
-                    // Precio original más pequeño en la esquina derecha inferior
                     Text(
-                        text = artwork.price.toString() + "€" + (if (artwork.unit!=null) "/" + artwork.unit else ""),
+                        text = "$price€${if (unit != null) "/$unit" else ""}",
                         style = MaterialTheme.typography.bodySmall,
-                        fontSize = 12.sp,  // Tamaño más pequeño
+                        fontSize = 12.sp,
                         color = MaterialTheme.colorScheme.onPrimaryContainer,
                         textDecoration = TextDecoration.LineThrough,
                         modifier = Modifier
-                            .align(Alignment.Bottom)  // Alinearlo en la parte inferior
-                            .padding(end = 8.dp)  // Alineado a la derecha
+                            .align(Alignment.Bottom)
+                            .padding(end = 8.dp)
                     )
                 }
             } else {
-                // Mostrar solo el precio original sin oferta
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -148,23 +136,19 @@ fun ProductPreview(artwork: Product, email: String) {
                         .padding(bottom = 4.dp)
                 ) {
                     Text(
-                        text = artwork.price.toString() + "€" + (if (artwork.unit!=null) "/" + artwork.unit else ""),
+                        text = "$price€${if (unit != null) "/$unit" else ""}",
                         style = MaterialTheme.typography.bodyLarge,
-                        fontSize = 18.sp,  // Tamaño más grande
+                        fontSize = 18.sp,
                         color = MaterialTheme.colorScheme.onPrimaryContainer,
                         fontWeight = FontWeight.SemiBold,
-                        modifier = Modifier
-                            // Esto lo sitúa en la izquierda
-                            .padding(start = 8.dp)
+                        modifier = Modifier.padding(start = 8.dp)
                     )
                 }
             }
-
-
         }
+
         if (showModal) {
-            //Toast.makeText(context, "hola0", Toast.LENGTH_SHORT).show()
-            ProductDetailModal(product = artwork, email, onDismiss = { showModal = false })
+            ProductDetailModal(product = product, email, onDismiss = { showModal = false })
         }
     }
 }
